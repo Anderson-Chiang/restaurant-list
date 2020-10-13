0445051fafb9995b9081e4e4f7d884f471100686
Anderson-Chiang <cachiang814@gmail.com>
2020-10-13 11:39:25 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
-const port = 3000
+
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
@@ -12,18 +15,28 @@ app.set('view engine', 'handlebars')
 // Set static file
 app.use(express.static('public'))
 
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  console.log('req.params.restaurant_id', req.params.restaurant_id)
+  // console.log('req.params.restaurant_id', req.params.restaurant_id)
   const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurant })
 })
 
 app.get('/search', (req, res) => {
-  console.log('req.query', req.query)
+  // console.log('req.query', req.query)
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
     return restaurant.name.toLocaleLowerCase().includes(keyword.toLowerCase())
@@ -31,6 +44,6 @@ app.get('/search', (req, res) => {
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
-app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+app.listen(3000, () => {
+  console.log('App is running on localhost:3000.')
 })
