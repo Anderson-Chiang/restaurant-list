679494c926cc4e14676b8c66dd0ece3c46dce0f0
Anderson-Chiang <cachiang814@gmail.com>
2020-10-19 00:14:54 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 //Set template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -26,15 +16,10 @@ app.set('view engine', 'hbs')
 
 // Set static file
 app.use(express.static('public'))
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
 
-
 app.use(routes)
-
-
 
 app.listen(port, () => {
   console.log(`The server is listening on http://localhost:${port}`)
